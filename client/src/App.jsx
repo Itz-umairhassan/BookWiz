f16caed16c593f819e1e3ad3e1fa7d457070e1d6
hp <tararhanzla783@gmail.co,m>
2024-03-04 17:05:29 +0500

    created Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
+import Header from './components/Header'
 import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
     <BrowserRouter className=' text-3xl text-red-400' >
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
