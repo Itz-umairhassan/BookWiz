18c1b8a2c39f7eb89d72043c294650f630a6ad0e
hp <tararhanzla783@gmail.co,m>
2024-03-04 16:38:59 +0500

    create Routes for all  Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div className=' text-3xl text-red-400' >
-      App
-      lorem124
-      </div>
+    <BrowserRouter className=' text-3xl text-red-400' >
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+         
+        
+      </Routes>
+      </BrowserRouter>
   )
 }
