148fbe92dd996883a2127a77e9ab8bb35b8250a3
umair hassan <umairhassangondal3@gmail.com>
2024-05-24 18:58:34 +0500

    folder + file basic setup

--- api/routes/fileRoutes.js
+++ api/routes/fileRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
 import File from "../models/fileModel.js";
+import Folder from "../models/folderModel.js";
+import mongodb from "mongodb"
 const router = express.Router();
 
 router.post('/upload' , async (req , res)=>{
     // suppose file is uploaded and a url is generated
-    const {fileName} = req.body;
+    const {folderId , fileName} = req.body;
     const url = "dummyurl.com";
 
     const newFile = new File({
@@ -14,8 +16,20 @@ router.post('/upload' , async (req , res)=>{
 
     try{
         const storedFile = await newFile.save();
+        
+        const folderObjId = new mongodb.ObjectId(folderId);
+
+        const response = await Folder.findByIdAndUpdate(
+            folderId,
+            {
+                $push:{files : new mongodb.ObjectId(storedFile["_id"])}
+            }
+        )
+
         res.status(200).json({
-            "message" : "file stored successfully"
+            "message" : "file stored successfully",
+            "file" : storedFile,
+            "response" : response
         })
 
     }catch(error){
