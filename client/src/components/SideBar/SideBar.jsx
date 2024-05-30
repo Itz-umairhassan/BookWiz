013ab6d1895ffa3cc9cf405cae03aa86c9d1b071
umair hassan <umairhassangondal3@gmail.com>
2024-05-30 06:00:14 +0500

    frontend fixes

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './sidebar.css'
 import { assets } from '../../assets/assets.js'
+import axios from 'axios'
+import {Folders} from "../SideBar/Folders.jsx"
+import Spinner from './Spinner.jsx'
+
 export const SideBar = () => {
-    const [extended, setExtended] = useState(false)
+    const [extended, setExtended] = useState(true);
+    const [folders , setFolders] = useState([]);
+    const [spin , setSpin] = useState(true);
+
+    // fecth folders and load them in sidebar....
+
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log("fetching data");
+            setSpin(true);
+            try {
+              const res = await axios.get('/api/user/folders',{
+                withCredentials: true
+              });
+
+              const dT = res.data.payload[0];
+              setFolders(dT);
+              console.log(dT);
+              setSpin(false);
+
+            } catch (error) {
+              console.error('Failed to fetch data:', error.response.data.message);
+              setSpin(false);
+            }
+          };
+      
+          console.log("side bar triggered");
+          fetchData();
+      },[]); // add dependencies if needed
 
     return (
         <div className='sidebar'>
@@ -11,24 +43,17 @@ export const SideBar = () => {
                 <img className='menu' src={assets.menu_icon} alt="" onClick={()=>setExtended(prev=>!prev)} />
                 <div className="new-chat">
                     <img src={assets.plus_icon} alt="" />
-                    {extended ? <p>new Chat</p> : null}
+                    {extended ? <p>Add New Folder</p> : null}
                 </div>
+               
                 {
-                    extended ?
-                        <div className="recent">
-                            <p className="recent-title">Recent  </p>
-                            <div className="recent-entry">
-                                <img src={assets.message_icon} alt="" />
-                                <p>what is react... </p>
-                            </div>
-                        </div>
-                        : null
+                   true ? <Spinner /> : folders.map((folder) => <Folders isExtended={extended} folderObj={folder} />)
                 }
             </div>
             <div className="bottom">
                 <div className="bottom-item recent-entry">
                     <img src={assets.question_icon} alt="" />
-                    {extended?<p>Help</p>:null}
+                    {extended?<p>Notes</p>:null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.history_icon} alt="" />
@@ -36,7 +61,7 @@ export const SideBar = () => {
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.send_icon} alt="" />
-                    {extended ?<p>Settings</p>:null}
+                    {extended ?<p>Logout</p>:null}
                 </div>
             </div>
         </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      }
+    }
   },
   plugins: [],
 }
