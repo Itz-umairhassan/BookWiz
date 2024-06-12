689e291467df02988d222c837c135a42351e5e07
umair hassan <umairhassangondal3@gmail.com>
2024-06-12 22:14:21 +0500

    css fixed + package added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom'
-import './assets/css/tailwind.output.css'
+import './assets/css/testing.css'
+//import 'normalize.css';
 import App from './App'
 import { SidebarProvider } from './context/SidebarContext'
 import ThemedSuspense from './components/ThemedSuspense'
 import { Windmill } from '@windmill/react-ui'
 import * as serviceWorker from './serviceWorker'
 import { ContextProvider } from './context/Context.jsx'
-
 
 ReactDOM.render(
   <SidebarProvider>
